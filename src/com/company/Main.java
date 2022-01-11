c7da614f306df74065ca6df161ffa8524cc91666
YanchunZhou <tifa1981@gmail.com>
2022-01-11 18:13:12 -0500

    made some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //
+        /*
+        go to the terminal and input the following to tell Git to stop tracking changes of contents under ignore file:
+
+***************************************
+git rm --cached "Java Programming.iml"*
+                                      *
+git rm -r --cached .idea              *
+                                      *
+git rm -r --cached .idea              *
+***************************************
+         */
     }
 }
